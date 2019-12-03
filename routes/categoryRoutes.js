9334efd2d26a7f353412420589204e06484c0c71
Your Name <jjay.cayabyab@gmail.com>
2019-12-02 22:10:27 -0700

    create category rout

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -20,4 +20,40 @@ module.exports = (app, connection) => {
       return sendSQLError(res);
     }
   });
+
+  app.get("/api/category", async (req, res) => {
+    const { categoryName } = req.query;
+
+    let categoryQuery = `
+      SELECT * FROM CATEGORY
+      WHERE CategoryName = ${connection.escape(categoryName)}
+    `;
+
+    let recipeQuery = `
+        SELECT rc.recipeId, rc.name, rc.description, rc.pictureUrl, AVG(rv.OverallRating) AS 'overallRating'
+        FROM RECIPE as rc LEFT JOIN REVIEW as rv
+        ON rv.RecipeID = rc.RecipeID
+        WHERE EXISTS
+        (
+          SELECT * FROM BELONGS_TO as bt
+          WHERE bt.CategoryName = ${connection.escape(categoryName)}
+          AND bt.RecipeID = rc.RecipeID
+        )
+        GROUP BY rc.recipeId, rc.name, rc.description, rc.pictureUrl
+       `;
+
+    try {
+      const [categories] = await connection.promise().query(categoryQuery);
+      const category = categories[0];
+
+      const [recipes] = await connection.promise().query(recipeQuery);
+
+      category.recipes = recipes;
+
+      res.send(camelcaseKeys(category));
+    } catch (error) {
+      console.log("SQL exception occurred: " + error);
+      return sendSQLError(res);
+    }
+  });
 };
