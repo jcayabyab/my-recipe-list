269b26e75e94642196abf192a4806c10edc75aa0
Minji Kim <minjikm19@gmail.com>
2019-12-03 23:35:51 -0700

    updated recipe page

--- client/src/components/RecipePage/RecipePage.js
+++ client/src/components/RecipePage/RecipePage.js
@@ -2,7 +2,15 @@ import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
-import { CssBaseline, Container, Button } from "@material-ui/core";
+import {
+  CssBaseline,
+  Container,
+  Button,
+  Avatar,
+  Box,
+  Grid,
+  Paper
+} from "@material-ui/core";
 import styled from "styled-components";
 
 const Body = styled(Container)`
@@ -11,7 +19,18 @@ const Body = styled(Container)`
 
 const Row = styled.div`
   display: flex;
-  align-items: flex-end;
+  align-items: center;
+
+  & > * {
+    margin: 0px 10px;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
   & > * {
     margin: 0px 10px;
   }
@@ -50,7 +69,32 @@ const RecipePage = ({ location }) => {
     <React.Fragment>
       <CssBaseline></CssBaseline>
       <Body>
-        <Row>RecipePage</Row>
+        <Column>
+          <Row>
+            <Avatar
+              variant="square"
+              style={{ width: "150px", height: "150px", borderRadius: 0 }}
+              alt="recipe pic"
+              src={recipe.pictureUrl}
+            />
+            <Grid item xs>
+              <div>
+                <span>
+                  <h2>{recipe.name}</h2>
+                </span>
+                <span>
+                  <Paper style={{ height: "100px" }}>
+                    {recipe.description}
+                  </Paper>
+                </span>
+              </div>
+            </Grid>
+          </Row>
+          <h2>&nbsp;&nbsp;Ingredient</h2>
+          <Paper style={{ height: "150px" }}>
+            .
+          </Paper>
+        </Column>
       </Body>
     </React.Fragment>
   ) : (
